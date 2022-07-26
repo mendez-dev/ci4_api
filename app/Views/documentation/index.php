4f017def3d012f049887c425f6abb2bfc74ffbc5
Wilber David Méndez Rodríguez <wilber.mendez@salud.gob.sv>
2022-07-26 10:49:40 -0600

    Limpieza de archivos y paquetes inecesarios

--- app/Views/documentation/index.php
+++ app/Views/documentation/index.php
@@ -4,16 +4,16 @@
   <head>
     <meta charset="UTF-8">
     <title>Documentación de la API</title>
-    <link rel="stylesheet" type="text/css" href="swagger/swagger-ui.css" />
-    <link rel="stylesheet" type="text/css" href="swagger/index.css" />
-    <link rel="icon" type="image/png" href="swagger/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="swagger/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="swagger/dist/swagger-ui.css" />
+    <link rel="stylesheet" type="text/css" href="swagger/dist/index.css" />
+    <link rel="icon" type="image/png" href="swagger/dist/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="swagger/dist/favicon-16x16.png" sizes="16x16" />
   </head>
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="swagger/swagger-ui-bundle.js" charset="UTF-8"> </script>
-    <script src="swagger/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+    <script src="swagger/dist/swagger-ui-bundle.js" charset="UTF-8"> </script>
+    <script src="swagger/dist/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
     
     <script>
       window.onload = function() {
